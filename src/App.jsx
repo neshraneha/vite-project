3dafbb0ba6fbe8f76b13fbaea6b79609cb8e30d8
Neha Kumari <neshraneha04@gmail.com>
2024-09-18 12:39:29 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="mt-5 w-[80%] m-auto">
+      <h1 className="text-5xl">This is React Vite Boilerplate</h1>
+      <h2 className="text-3xl">{import.meta.env.VITE_API_KEY}</h2>
+      <button className="px-5 py-2 bg-red-300 rounded ">Explore</button>
+    </div>
+  );
+};
+
+export default App;
